bf8d4bf5c5609a2e4b7b61a13bd08271b010bce3
wookjongkim <121083077+wookjongkim@users.noreply.github.com>
2023-06-19 08:39:51 +0900

    Initial commit

---
+++ src/main/java/com/project/catchtable/CatchTableApplication.java
@@ -0,0 +1,13 @@
+package com.project.catchtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/catchtable/CatchTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.catchtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
